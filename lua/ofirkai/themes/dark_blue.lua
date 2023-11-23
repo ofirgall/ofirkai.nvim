a6cdfb58553c5906f8bc797ee3d7b5bdcc52ce38
Ofir Gal <ofir@gal.software>
2023-11-23 23:18:53 +0200

    dark_blue: blue line num colors

--- lua/ofirkai/themes/dark_blue.lua
+++ lua/ofirkai/themes/dark_blue.lua
@@ -12,8 +12,8 @@ M.scheme = {
 	telescope_bg = '#0f1018',
 
 	cursor_line_bg = '#1b1f21',
-	cursor_linenr_fg = '#e0e0e0',
-	line_fg = '#7a7a7a',
+	cursor_linenr_fg = '#909ef5',
+	line_fg = '#363a5e',
 	winbar_bg = '#171b1d',
 	white = '#edede5',
 	-- visual_bg = '#30303c',
